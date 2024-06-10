76279934030ddd638a60a8e68f1dae90ce0b71a0
dmac <DaraMac@users.noreply.github.com>
2024-06-10 07:49:28 +0100

    Start Mult

---
+++ projects/04/mult/Mult.asm
@@ -0,0 +1,21 @@
+// Do multiplication by repeatedly adding one value and decrementing the other
+
+// Get value that will be added on
+@R0
+D=M
+
+@a
+M=D
+
+
+// Get value that will be decremented for the loop
+@R1
+D=M
+
+@n
+M=D
+
+// The multiplication loop
+(LOOP)
+@n
+
